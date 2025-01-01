3201f5086e4efa610f25422649241616241f8ad1
Athulkrishna-S <athulkrishna768@gmail.com>
2025-01-01 13:06:31 +0530

    updated wave_to_csv

--- WaveToEEG/wave_to_csv.py
+++ WaveToEEG/wave_to_csv.py
@@ -1,34 +1,53 @@
 import wave
 import numpy as np
 import pandas as pd
-# Input and output file paths
-wav_file = 'Abhijith_eye_up.wav'   # Replace with your WAV file path
-csv_file = 'output.csv'  # Replace with your desired output CSV file path
 
-# Open the WAV file
-with wave.open(wav_file, 'r') as wav:
-    # Extract parameters
-    n_channels = wav.getnchannels()
-    sample_width = wav.getsampwidth()
-    frame_rate = wav.getframerate()
-    n_frames = wav.getnframes()
+import os 
+import glob
 
-    print(f"Channels: {n_channels}")
-    print(f"Sample Width: {sample_width}")
-    print(f"Frame Rate: {frame_rate}")
-    print(f"Frames: {n_frames}")
+folder_path = r'datasets_wav_files'
 
-    # Read all frames as bytes
-    frames = wav.readframes(n_frames)
+wav_files = glob.glob(os.path.join(folder_path,'*.wav'))
 
-    # Convert bytes to numpy array
-    dtype = np.int16 if sample_width == 2 else np.int8
-    audio_data = np.frombuffer(frames, dtype=dtype)
+output_folder_path = r'datasets_csv_files'
 
-    # Handle stereo audio (multiple channels)
-    if n_channels > 1:
-        audio_data = audio_data.reshape(-1, n_channels)
+# Ensure the output folder exists
+os.makedirs(output_folder_path, exist_ok=True)
 
-# Save to CSV
-pd.DataFrame(audio_data).to_csv(csv_file, index=False, header=False)
-print(f"Saved data to {csv_file}")
+for wav_file in wav_files:
+
+    # Extract the file name without extension
+    file_name = os.path.splitext(os.path.basename(wav_file))[0]
+
+    # Construct output CSV file path
+    csv_file = os.path.join(output_folder_path,f'{file_name}.csv')
+
+
+    with wave.open(wav_file, 'r') as wav:
+        # Extract parameters
+        n_channels = wav.getnchannels()
+        sample_width = wav.getsampwidth()
+        frame_rate = wav.getframerate()
+        n_frames = wav.getnframes()
+
+        print(f"Channels: {n_channels}")
+        print(f"Sample Width: {sample_width}")
+        print(f"Frame Rate: {frame_rate}")
+        print(f"Frames: {n_frames}")
+
+        # Read all frames as bytes
+        frames = wav.readframes(n_frames)
+
+        # Convert bytes to numpy array
+        dtype = np.int16 if sample_width == 2 else np.int8
+        audio_data = np.frombuffer(frames, dtype=dtype)
+
+        # Handle stereo audio (multiple channels)
+        if n_channels > 1:
+            audio_data = audio_data.reshape(-1, n_channels)
+
+    #Save to CSV
+    pd.DataFrame(audio_data).to_csv(csv_file, index=False, header=False)
+    print(f"Saved data to {csv_file}")
+    
+    print("="*100)
